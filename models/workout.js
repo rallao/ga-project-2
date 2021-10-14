c4fb959c531445f6f21bcec41969ab1e1c4ba04d
rallao85@gmail.com <rallao85@gmail.com>
2021-10-13 23:15:23 -0300

    wip: fix workout model bug

--- models/workout.js
+++ models/workout.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 
 // Create the Schema
 const workoutSchema = new Schema ({
-    title: { type: String, required:true}
+    title: { type: String, required:true},
     notes: { type: String, required:false}
-})
+}, { timestamps: true});
 
 // Export the model
 const Workout = mongoose.model('Workout', workoutSchema);
